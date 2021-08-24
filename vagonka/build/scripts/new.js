32dc5ec7c5b8232ea8b033eb5faec00ea3e79ce0
Ruslan Belozertsev <38186770+ruslanmmr@users.noreply.github.com>
2021-08-24 14:04:48 +0300

    fix

--- vagonka/build/scripts/new.js
+++ vagonka/build/scripts/new.js
@@ -11,7 +11,7 @@ $(document).ready(function () {
 });
 
 function homeBanner() {
-  var $slider = $('.home-banner'),
+  var $slider = $('.home-banner .owl-carousel'),
       arrowPrev = '<svg class="icon" viewBox="0 0 10.5 18.1"><path stroke="none" d="M9,0l1.4,1.4L2.8,9l7.6,7.6L9,18.1L0,9C0,9,9.1,0,9,0z"></path></svg>',
       arrowNext = '<svg class="icon" viewBox="0 0 10.5 18.1"><path stroke="none" d="M1.4,18.1L0,16.7l7.6-7.6L0,1.5L1.4,0l9,9.1C10.4,9.1,1.3,18.1,1.4,18.1z"></path></svg>';
 
@@ -23,8 +23,8 @@ function homeBanner() {
       dots: true,
       items: 1,
       lazyLoad: true,
-      autoplay: false,
-      autoplayTimeout: 5000,
+      autoplay: true,
+      autoplayTimeout: (+$slider.data('autoplay-timeout') || 5) * 1000,
       navText: [arrowPrev, arrowNext]
     });
   }
