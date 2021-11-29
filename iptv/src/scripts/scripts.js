db0d62a3af2f2719b579984318bb341911b06782
Ruslan Belozertsev <38186770+ruslanmmr@users.noreply.github.com>
2021-11-29 14:42:02 +0200

    eddited

---
+++ iptv/src/scripts/scripts.js
@@ -0,0 +1,101 @@
+const breakpoints = {
+  sm: 576,
+  md: 768,
+  lg: 1024,
+  xl: 1280,
+  xxl: 1600
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+  CustomInteractionEvents.init();
+  autoRenewalCheck();
+});
+
+const CustomInteractionEvents = Object.create({
+  targets: {
+    value: 'a, button, [data-custom-interaction]'
+  },
+  touchEndDelay: {
+    value: 100
+  }, 
+  init() {
+    this.events = (event) => {
+      let $targets = [];
+      $targets[0] = event.target!==document?event.target.closest(this.targets.value):null;
+      let $element = $targets[0], i = 0;
+  
+      while($targets[0]) {
+        $element = $element.parentNode;
+        if($element!==document) {
+          if($element.matches(this.targets.value)) {
+            i++;
+            $targets[i] = $element;
+          }
+        } 
+        else {
+          break;
+        }
+      }
+  
+      //touchstart
+      if(event.type=='touchstart') {
+        this.touched = true;
+        if(this.timeout) clearTimeout(this.timeout);
+        if($targets[0]) {
+          for(let $target of $targets) $target.setAttribute('data-touch', '');
+        }
+      } 
+      //touchend
+      else if(event.type=='touchend' || (event.type=='contextmenu' && this.touched)) {
+        this.timeout = setTimeout(() => {this.touched = false}, 500);
+        if($targets[0]) {
+          setTimeout(()=>{
+            for(let $target of $targets) {
+              $target.removeAttribute('data-touch');
+            }
+          }, this.touchEndDelay.value)
+        }
+      } 
+      //mouseenter
+      if(event.type=='mouseenter' && !this.touched && $targets[0] && $targets[0]==event.target) {
+        $targets[0].setAttribute('data-hover', '');
+      }
+      //mouseleave
+      else if(event.type=='mouseleave' && !this.touched && $targets[0] && $targets[0]==event.target) {
+        $targets[0].removeAttribute('data-click');
+        $targets[0].removeAttribute('data-hover');
+      }
+      //mousedown
+      if(event.type=='mousedown' && !this.touched && $targets[0]) {
+        $targets[0].setAttribute('data-click', '');
+      } 
+      //mouseup
+      else if(event.type=='mouseup' && !this.touched  && $targets[0]) {
+        $targets[0].removeAttribute('data-click');
+      }
+    }
+    document.addEventListener('touchstart',  this.events);
+    document.addEventListener('touchend',    this.events);
+    document.addEventListener('mouseenter',  this.events, true);
+    document.addEventListener('mouseleave',  this.events, true);
+    document.addEventListener('mousedown',   this.events);
+    document.addEventListener('mouseup',     this.events);
+    document.addEventListener('contextmenu', this.events);
+  }
+})
+
+function autoRenewalCheck() {
+  document.addEventListener('change', (event) => {
+    const $target = event.target;
+    const $parent = $target.closest('.auto-renewal-element');
+    if (!$parent) return;
+
+    const $text = $parent.querySelector('.auto-renewal-element__text strong');
+
+    if ($target.checked) {
+      $text.textContent = 'включено';
+    } else {
+      $text.textContent = 'не включено';
+    }
+  })
+}
